131a2f15b8b271066176d5127201fb8ba4d1de4f
David Andrews <dandrewsuk82@gmail.com>
2020-08-12 18:31:31 +0100

    Autogit: 12-08-2020 18:31

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,6 +1,7 @@
 // Hero.js
 import React from "react";
 import { Box, Typography, Button, Container, Grid } from "@material-ui/core";
+import illustration from "../study.svg";
 
 const Hero = () => {
   return (
@@ -12,8 +13,23 @@ const Hero = () => {
       position="relative"
       bgcolor="primary.dark"
     >
+      <Box
+        display="flex"
+        top={0}
+        right={0}
+        bottom={0}
+        left={0}
+        justifyContent="flex-end"
+        alignItems="flex-end"
+        position="absolute"
+      >
+        <img
+          src={illustration}
+          style={{ maxWidth: "100%", maxHeight: "100%" }}
+        />
+      </Box>
       <Container>
-        <Box align="center" position="relative" borderRadius="borderRadius">
+        <Box align="left" position="relative" borderRadius="borderRadius">
           <Typography variant="h2">Make Memorizing Fun!</Typography>
           <Typography variant="subtitle1" paragraph>
             From learning a new language to revising for an exam, use{" "}
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,15 @@
 import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
-import { AppBar, Toolbar, Typography, Box, Button } from "@material-ui/core";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Box,
+  Button,
+  Hidden,
+  IconButton,
+} from "@material-ui/core";
+import { Menu } from "@material-ui/icons";
 import { setNewGameOpen } from "../state/dialogs/actions";
 
 const Navbar = ({ dispatch }) => {
@@ -28,18 +37,25 @@ const Navbar = ({ dispatch }) => {
       <Toolbar>
         <Typography variant="h5">Match It!</Typography>
         <Box flexGrow={1} />
-        <Button
-          onClick={handleClick}
-          id="new-game"
-          style={{ marginRight: 15 }}
-          variant="contained"
-          color="primary"
-        >
-          New Game
-        </Button>
-        <Button variant="contained" color="secondary">
-          New Collection
-        </Button>
+        <Hidden smDown>
+          <Button
+            onClick={handleClick}
+            id="new-game"
+            style={{ marginRight: 15 }}
+            variant="contained"
+            color="primary"
+          >
+            New Game
+          </Button>
+          <Button variant="contained" color="secondary">
+            New Collection
+          </Button>
+        </Hidden>
+        <Hidden mdUp>
+          <IconButton edge="end" color="inherit">
+            <Menu />
+          </IconButton>
+        </Hidden>
       </Toolbar>
     </AppBar>
   );
